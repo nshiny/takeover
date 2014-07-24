c744e0f8d7400f3e80ab23266a215e635d56db12
samtb <sbrelsfoard@sbrelsfoard.local>
2014-07-24 19:47:21 -0400

    Adding duke_or_die_bot

--- bots/duke_or_die_bot.py
+++ bots/duke_or_die_bot.py
@@ -2,20 +2,20 @@ from interface import *
 
 import random
 
-class SamBot(Bot):
+class DukeOrDie(Bot):
     def __init__(self, identifier):
         self.identifier = identifier
-        self.samTurn = 0
+        self.dukeTurn = 0
         
     def start(self):
-        self.samTurn = 0
+        self.dukeTurn = 0
 
     def update_state(self, states, hidden):
         self.states = states
         self.hidden = hidden
         
     def take_action(self):
-        self.samTurn += 1 # every turn, increment the turn number
+        self.dukeTurn += 1 # every turn, increment the turn number
         active = [x.identifier for x in self.states if len(x.flipped) < 2]
         active.remove(self.identifier)
         target = random.choice(active)
@@ -24,33 +24,15 @@ class SamBot(Bot):
 
         if coins >= 7: # coup if we have 7 coins
             return TargetedAction(Action.coup, target)
-        elif self.samTurn == 1: # always tax on first turn
+        elif self.dukeTurn % 2 == 0:
             return Action.tax
-        elif Character.duke in self.hidden: # if we have the duke, tax
-            return Action.tax
-        elif Character.assassin in self.hidden and coins >= 3: # if we have the assasin and we have between 3 and 6 coin, assassinate
-            return TargetedAction(Action.assassinate, target)
-        elif Character.captain in self.hidden: # if we have the captain, extort
-            return TargetedAction(Action.extort, target)
-        elif Character.contessa not in self.hidden: # if we don't have a contessa, exchange until we do
+        elif self.dukeTurn % 2 != 0 and Character.duke not in self.hidden:
             return Action.exchange
         else:
-            return Action.income
+            return Action.tax
 
     def challenge(self, actor, action, character, target):
-        if action == Action.exchange and self.samTurn > 7 and Character.ambassador in self.hidden:
-            return True
         if action == Action.foreign_aid:
-            if Character.duke in self.hidden:
-               	return True
-        if action == Action.extort and Character.captain in self.hidden:
-            return True
-        if action == Action.tax:
-            if Character.duke in self.hidden:
-                return True
-        if action == Action.block and character == Character.contessa and Character.contessa in self.hidden:
-            return True
-        if action == Action.block and character == Character.contessa and len(self.hidden) > 1:
             return True
         return False
         
@@ -60,24 +42,21 @@ class SamBot(Bot):
 
         if len(self.hidden) > 1 and action.blockable(self.hidden[1]):
             return self.hidden[1]
-            
-        if action == Action.assassinate and len(self.hidden) > 1:
-            return Character.contessa    
 
         return None
 
     def _prioritize(self, characters):
         prioritized = []
         prioritized.extend(
+            [Character.duke] * characters.count(Character.duke))
+        prioritized.extend(
+            [Character.ambassador] * characters.count(Character.ambassador))
+        prioritized.extend(
             [Character.contessa] * characters.count(Character.contessa))
         prioritized.extend(
             [Character.assassin] * characters.count(Character.assassin))
         prioritized.extend(
-            [Character.duke] * characters.count(Character.duke))
-        prioritized.extend(
             [Character.captain] * characters.count(Character.captain))
-        prioritized.extend(
-            [Character.ambassador] * characters.count(Character.ambassador))
         
         return prioritized  
 
@@ -86,16 +65,16 @@ class SamBot(Bot):
         return prioritized[-2:]
 
     def flip(self):
-        if Character.ambassador in self.hidden:
-            return Character.ambassador
-        if Character.captain in self.hidden:
-            return Character.captain
-        if Character.duke in self.hidden:
-            return Character.duke
         if Character.assassin in self.hidden:
             return Character.assassin
         if Character.contessa in self.hidden:
             return Character.contessa
+        if Character.captain in self.hidden:
+            return Character.captain
+        if Character.ambassador in self.hidden:
+            return Character.ambassador
+        if Character.duke in self.hidden:
+            return Character.duke
 
     def reveal(self, challenger, action, character, taret):
         if character in self.hidden:
@@ -104,4 +83,4 @@ class SamBot(Bot):
         return self.flip()
 
 def make_bot(identifier):
-    return SamBot(identifier)
+    return DukeOrDie(identifier)
